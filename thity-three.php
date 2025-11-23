a10f39f6c744afc17bcd245f17a289a06fff4518
S. Schotvanger <s.schotvanger@gmail.com>
2025-11-23 16:39:31 +0100

    file types

--- thity-three.php
+++ thity-three.php
@@ -14,7 +14,7 @@
  */
 
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
@@ -26,41 +26,72 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 function thirtyThreeInit(): void
 {
-	if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
+	if (function_exists('wp_register_block_types_from_metadata_collection')) {
 		wp_register_block_types_from_metadata_collection(
 			path: __DIR__ . '/build',
 			manifest: __DIR__ . '/build/blocks-manifest.php'
 		);
 	}
 }
-add_action( 'init', 'thirtyThreeInit', 20 );
+
+add_action('init', 'thirtyThreeInit', 20);
 
 
 /**
  * Make sure translations are loaded for all blocks.
  */
 
-add_action( 'enqueue_block_editor_assets', 'multiblockRegisterScriptTranslations' );
-add_action( 'enqueue_block_assets', 'multiblockRegisterScriptTranslations' );
+add_action('enqueue_block_editor_assets', 'multiblockRegisterScriptTranslations');
+add_action('enqueue_block_assets', 'multiblockRegisterScriptTranslations');
 
 function multiblockRegisterScriptTranslations(): void
 {
-	$base_path  = plugin_dir_path( __FILE__ );
-	$asset_dirs = glob( $base_path . 'build/*', GLOB_ONLYDIR );
+	$base_path = plugin_dir_path(__FILE__);
+	$asset_dirs = glob($base_path . 'build/*', GLOB_ONLYDIR);
 
-	foreach ( $asset_dirs as $block_path ) {
-		$block_name = basename( $block_path );
+	foreach ($asset_dirs as $block_path) {
+		$block_name = basename($block_path);
 
 		// Editor script
 		$editor_handle = "multiblock-{$block_name}-editor-script";
-		if ( wp_script_is( $editor_handle, 'registered' ) ) {
-			wp_set_script_translations( $editor_handle, 'multiblock', $base_path . 'languages' );
+		if (wp_script_is($editor_handle, 'registered')) {
+			wp_set_script_translations($editor_handle, 'multiblock', $base_path . 'languages');
 		}
 
 		// View script
 		$view_handle = "multiblock-{$block_name}-view-script";
-		if ( wp_script_is( $view_handle, 'registered' ) ) {
-			wp_set_script_translations( $view_handle, 'multiblock', $base_path . 'languages' );
+		if (wp_script_is($view_handle, 'registered')) {
+			wp_set_script_translations($view_handle, 'multiblock', $base_path . 'languages');
 		}
 	}
 }
+
+add_filter('upload_mimes', 'add3MFUploads');
+
+add_filter('wp_check_filetype_and_ext', 'checkFilterMedia', 10, 4);
+
+
+function add3MFUploads($mimeTypes): array
+{
+	$mimeTypes['3mf'] = 'model/3mf';
+	// Add the 3MF MIME type.
+	return $mimeTypes;
+}
+
+/**
+ * Add 3MF file type to the allowed file types
+ *
+ * @return array
+ */
+
+function checkFilterMedia(array $types, $file, $filename, $mimes)
+{
+	if (str_contains($filename, '.3mf') === true) {
+		$types['ext'] = '3mf';
+		$types['type'] = 'model/3mf';
+	}
+
+	return $types;
+
+}//end checkFilterMedia()
+
